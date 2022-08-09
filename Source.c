4d4d01ed86e9ffa5efaffc02913470b3156e8bca
ehddjs95@naver.com <janghanjinn@naver.com>
2022-08-09 21:46:57 +0900

    220809

--- Source.c
+++ Source.c
@@ -10,15 +10,16 @@ void main()
 				int : 정수를 저장하는 자료형
 				float : 실수를 저장하는 자료형
 
-		변수(*box)
+		변수(box)*
 		대입 연산자 "="은 우측 피연산자 값을 좌측 피연산자에 저장한다
-
-
-
-		int box = 100;
-		printf("%d\n", box);
-		printf("안녕하세요.");
 	*/
+	//ex)
+	int box = 100;
+	char alphabet = 'A';
+	float pi = 3.14;
 
-	
+	printf("안녕하세요.\n");
+	printf("%d\n", box);
+	printf("%f\n", pi);
+	printf("%c\n", alphabet);
 }
