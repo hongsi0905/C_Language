cc0b435bfe9f624f9d6790e431fc4e9d0e645fe6
hongsi0905 <https://github.com/hongsi0905/C_Language.git>
2022-09-02 21:47:07 +0900

    220902 구조체

---
+++ Class data/220901 포인터 배열.h
@@ -0,0 +1,111 @@
+#pragma once
+#include <stdio.h>
+
+void main()
+{
+	// 포인터 배열
+	/*
+		배열의 요소로 포인터 변수를 가지는 배열
+
+
+	int num1 = 10, num2 = 20, num3 = 30;
+
+	int* array[] = { &num1, &num2, &num3 };
+
+	for (int i = 0;i < 3;i++)
+	{
+		printf("array 포인터의 값 : %p\n", array[i]);
+		printf("arry 포인터가 가리키는 값 : %d\n", *array[i]);
+	}
+	*/
+
+	// 포인터 문자열
+	/*
+	 const char* data[3] = { "Apple","Banana","Melon" }; //시작주소만 알면 %s로 문자 출력
+
+	 data[0] = "Cherry"; // 기존의 문자열에 덮어씌우는 게 아닌 참조 위치를 바꿈
+
+	 for (int i = 0;i < 3;i++)
+	 {
+		 printf("%s\n", data[i]); // %s : '\0'(NULL) 문자를 만날 때까지 출력하는 서식 지정자
+	 }
+	 */
+
+	 // ASCII 코드
+	 /*
+		 미국 표준화 협회가 제정한 정보 교환용 표준코드
+		 영문 알파벳을 사용하는 대표적인 문자 인코딩
+
+
+	 if ('A' < 'B')
+	 {
+		 for (int i = 0;i < 26;i++)
+		 {
+			 char alphabet = 'A' + i;
+			 printf("%c ", alphabet);
+		 }
+	 }
+	 */
+
+	 // 공약수
+	 /*
+		 두 개의 변수(int)를 입력
+		 A와 B 변수의 공통된 약수를 출력
+
+	 int A = 0, B = 0;
+	 printf("변수를 입력해주세요 : ");
+	 scanf_s("%d %d", &A, &B);
+
+	 printf("A의 약수 : ");
+	 for (int i = 1;i <= A;i++)
+	 {
+		 if (A % i == 0)
+		 {
+			 printf("%d ", i);
+		 }
+	 }
+
+	 printf("\nB의 약수 : ");
+	 for (int i = 1;i <= B;i++)
+	 {
+		 if (B % i == 0)
+		 {
+			 printf("%d ", i);
+		 }
+	 }
+	 printf("\nA와 B의 공약수 : ");
+
+	 for (int i = 1;i <= A && i <= B;i++)
+	 {
+		 if (A % i == 0 && B % i == 0)
+		 {
+			 printf("%d ", i);
+		 }
+	 }
+	 */
+
+	 // 완전수
+	 /*
+		 자기 자신을 제외한 양의 약수(진약수)를 더했을 때 자기 자신이 되는 양의 정수를 말한다.
+		 또는 모든 양의 약수를 더했을 때 자기 자신의 2배가 되는 수라고도 한다. 6, 28 ,496...
+
+
+	 int a;
+	 int sum = 0;
+
+	 printf("완전수를 입력하세요(값이 나오지 않으면 완전수가 아닙니다.) : ");
+	 scanf_s("%d", &a);
+
+	 for (int i = 1;i <= a;i++)
+	 {
+		 if (a % i == 0)
+		 {
+			 sum += i;
+			 if (sum == a)
+			 {
+				 printf("%d 은/는 완전수 입니다.", sum);
+			 }
+		 }
+	 }
+	 */
+}
--- Source.c
+++ Source.c
@@ -1,112 +1,116 @@
-#include <stdio.h>
+#include <stdio.h> // < > 라이브러리 헤더 파일을 가져올 때 사용
+#include "Calculator.h" // "" 사용자 정의 헤더 파일을 가져올 때 사용
+
+//매크로
+/*
+	매크로에서는 ;가 필요하지 않다.
+	매크로와 매크로 함수는 자료형이 없다. > 메모리 공간이 확보되지 않는다.
+	매크로 변수는 상수이고, 메모리 공간이 없기 때문에 값을 변경할 수 없다.
+	단점 : 문제가 생겼을 때 디버깅이 되지 않는다.
+*/
+#define PI 3.14
+#define Solution(x, y) x * y
+#define ANDROID 1
+#define IOS 0
+
+// 구조체
+/*
+	구조체를 선언하기 전에
+	구조체는 메모리 공간이 생성되지 않으므로
+	구조체 내부에 있는 데이터를 초기화할 수 없다.
+*/
+struct Character
+{
+	int health;
+	float weight;
+	const char* name;
+};
 
 void main()
 {
-	// 포인터 배열
+	// 전처리기
 	/*
-		배열의 요소로 포인터 변수를 가지는 배열
-	
+		프로그램이 컴파일되기 이전에 프로그램에 대한 사전 처리하는 과정
 
-	int num1 = 10, num2 = 20, num3 = 30;
+	int result = Function(10, 20);
 
-	int* array[] = { &num1, &num2, &num3 };
-
-	for (int i = 0;i < 3;i++)
-	{
-		printf("array 포인터의 값 : %p\n", array[i]); 
-		printf("arry 포인터가 가리키는 값 : %d\n", *array[i]);
-	}
+	printf("result변수의 값 : %d\n", result);
 	*/
 
-	// 포인터 문자열
+	// 매크로
 	/*
-	 const char* data[3] = { "Apple","Banana","Melon" }; //시작주소만 알면 %s로 문자 출력
 	
-	 data[0] = "Cherry"; // 기존의 문자열에 덮어씌우는 게 아닌 참조 위치를 바꿈
-
-	 for (int i = 0;i < 3;i++)
-	 {
-		 printf("%s\n", data[i]); // %s : '\0'(NULL) 문자를 만날 때까지 출력하는 서식 지정자
-	 }
-	 */
-
-	// ASCII 코드
-	/*
-		미국 표준화 협회가 제정한 정보 교환용 표준코드
-		영문 알파벳을 사용하는 대표적인 문자 인코딩
-	
-
-	if ('A' < 'B')
-	{
-		for (int i = 0;i < 26;i++)
-		{
-			char alphabet = 'A' + i;
-			printf("%c ", alphabet);
-		}
-	}
+	int a = 1;
+	int b = 2;
+	printf("PI의 값 : %f\n", PI);
+	printf("Solution 함수 결과 : %d\n", Solution(a+1, a+2));
 	*/
 
-	// 공약수
+	// 조건부 컴파일
 	/*
-		두 개의 변수(int)를 입력
-		A와 B 변수의 공통된 약수를 출력
+		조건에 따라 코드의 일정 부분을 컴파일할지 안 할지 결정
+		조건부 컴파일은 #endif를 사용해서 끝내야 한다.
 	
-	int A = 0, B = 0;
-	printf("변수를 입력해주세요 : ");
-	scanf_s("%d %d", &A, &B);
+#if IOS
+	printf("안드로이드 기기입니다."); // 참 (밝은색) 거짓 (짙은색)
+#elif ANDROID
+	printf("두 번째 조건입니다."); // 참 (밝은색) 거짓 (짙은색)
+#else
+	printf("2개의 조건이 틀렸습니다."); // 참 (밝은색) 거짓 (짙은색)
+#endif
+*/
 
-	printf("A의 약수 : ");
-	for (int i = 1;i <= A;i++)
-	{
-		if (A % i == 0)
-		{
-			printf("%d ", i);
-		}
-	}
-
-	printf("\nB의 약수 : ");
-	for (int i = 1;i <= B;i++)
-	{
-		if (B % i == 0)
-		{
-			printf("%d ", i);
-		}
-	}
-	printf("\nA와 B의 공약수 : ");
+	// 구조체
+	/*
+		여러 개의 변수를 하나의 집합으로 구조화한 다음 하나의 객체를 생성하는 것
 	
-	for (int i = 1;i <= A && i <= B;i++)
-	{
-		if (A % i == 0 && B % i == 0)
-		{
-			printf("%d ", i);
-		}
-	}
+	struct Character leesin;
+	leesin.health = 100;
+	leesin.name = "용의 분노";
+	leesin.weight = 80.5f;
+
+	printf("리신의 체력 : %d\n", leesin.health);
+	printf("리신의 궁극기 : %s\n", leesin.name);
+	printf("리신의 무게 : %.2f kg\n", leesin.weight);
+	
+	// 구조체에 초기화 리스트 사용 시
+	// 구조체 위에서 선언된 변수의 순서로 정의되어야 한다.
+	struct Character BlackMage = { 1000,12.25f,"HardBoss"};
+	printf("검마 체력 : %d\n", BlackMage.health);
+	printf("검마 난이도 : %s\n", BlackMage.name);
+	printf("검마 결정석 : %.2f\n", BlackMage.weight);
 	*/
 
-	// 완전수
+	// 과잉수
 	/*
-		자기 자신을 제외한 양의 약수(진약수)를 더했을 때 자기 자신이 되는 양의 정수를 말한다.
-		또는 모든 양의 약수를 더했을 때 자기 자신의 2배가 되는 수라고도 한다. 6, 28 ,496...
+		자연수 중에 자기 자신을 제외한 양의 약수를
+		모두 더했을 때 자기 자신보다 더 커지는 수이다.
+		ex 12, 18, 20, 24, ...
 	*/
 
-	int a;
+	int num;
 	int sum = 0;
-
-	printf("완전수를 입력하세요(값이 나오지 않으면 완전수가 아닙니다.) : ");
-	scanf_s("%d", &a);
-
-	for (int i = 1;i <= a;i++)
+	printf("자연수를 입력해주세요.");
+	scanf_s("%d", &num);
+	for (int x = 1;x <= num;x++)
 	{
-		if (a % i == 0)
+		printf("%d : ", x);
+		for (int i = 1;i < x;i++)
 		{
-			sum += i;
-			if (sum == a)
+			if (x % i == 0)
 			{
-				printf("%d 은/는 완전수 입니다.", sum);
+				sum += i;
 			}
 		}
+		if (sum > x)
+		{
+			printf("과잉수입니다.\n");
+		}
+		else
+		{
+			printf("과잉수 아닙니다.\n");
+		}
+		sum = 0;
 	}
-
-
 
 }
