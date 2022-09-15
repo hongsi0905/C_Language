d90dc7e132c2296f9c1356d1c48f231462bc41dd
hongsi0905 <https://github.com/hongsi0905/C_Language.git>
2022-09-15 21:48:23 +0900

    파일 입출력

---
+++ Class data/220915 열거형.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <stdio.h>
+
+// 열거형(enum)
+/*
+	반드시 하나의 값만 가지게 될 변수들을 모아놓은 집합체
+	열거형은 정수값으로 정의할 수 없다.
+
+//Day 열거형 생성
+enum State
+{
+	/*
+	// 열거형 초기값을 명시하지 않으면 가장 처음에 있는 열거형 멤버 변수는 0이라는 값으로 초기화
+	SunDay, // 첫 번째 열거형 멤버 변수
+	MondDay = 20, // 열거형 값 변경가능, 값 설정이후의 변수또한 값 변경
+	TuesDay,
+	WednesDay
+
+	IDLE,
+	ATTACK,
+	JUMP,
+	DEATH
+};
+*/
+
+int main()
+{
+	// 열거형
+	/*
+	// 열거형 변수 선언
+	enum State state;
+
+	// 시작은 캐릭터를 대기상태로 지정
+	state = IDLE;
+
+	switch (state)
+	{
+	case IDLE: printf("대기 상태");
+		break;
+	case ATTACK: printf("공격");
+		break;
+	case JUMP: printf("점프");
+		break;
+	case DEATH: printf("죽음");
+		break;
+	}
+	*/
+
+
+
+	return 0;
+}
